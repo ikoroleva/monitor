98c3ff03205b560d797012a9aa1ceb3018c7675a
ikoroleva <prsml.ellisson@gmail.com>
2022-09-07 23:36:48 +0200

    cleaned up

--- procmon/Program.cs
+++ procmon/Program.cs
@@ -1,7 +1,4 @@
-﻿//todo: several instances
-//todo: logging information to file
-
-
+﻿using System;
 using System.Diagnostics;
 
 namespace procmon
@@ -10,15 +7,15 @@ namespace procmon
     {
         static void Help(string[] args)
         {
-
-            System.Console.WriteLine("\nprocmon [processName, lifetime, frequency]:\n processName - (obligatory, string) name of the proccess \n lifetime - (obligatory, int) maximum process lifetime in minutes \n frequency - (obligatory, int) monitoring frequency for this process\n");
-
+            Console.WriteLine("\nprocmon [processName] [lifetime] [frequency]:\n processName - (obligatory, string) name of the process \n lifetime - (obligatory, int) maximum process lifetime in minutes \n frequency - (obligatory, int) monitoring frequency for this process\n");
         }
-        static void Main(string[] args)
 
+        // todo: in order to use this as an utility that could be better to get command and run each monitor in a separate thread inside the single instance
+        // todo: maybe log information into file
+        static void Main(string[] args)
         {
-            //validate input arguments, show help message if input is incorrect
-            //todo: parse arguments lifetime and frequency as a double and then round them when counting lifetime and interval 
+            // validate input arguments, show help message if input is incorrect
+            // todo: parse arguments lifetime and frequency as a double and then round them when counting lifetime and interval 
             if (args.Length < 3 ||
                 args[0] == "--h" ||
                 args[0] == "--help" ||
@@ -30,41 +27,42 @@ namespace procmon
                 return;
             }
 
+            Console.WriteLine($"Monitor for process '{args[0]}' is starting.\n It would check for '{args[0]}' every {monFrequency} minutes. \nAfter {maxLifeTime} minutes of lifetime all '{args[0]}' processed will be killed. \nPress ESC to stop");
 
-            Console.WriteLine($"Monitor for proccess '{args[0]}' is starting.\n It would check for '{args[0]}' every {monFrequency} minutes. \nAfter {maxLifeTime} minutes of lifetime all '{args[0]}' processed will be killed. \nPress ESC to stop");
-
-
-            //proccess is running until key ESC woudln't pressed
+            // process is running until key ESC pressed
             while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Escape))
             {
-                int intInterval = monFrequency * 60000; //set interval in minutes
-                Process[] procs = Process.GetProcessesByName(args[0]); //find proccess by name
+                // set interval in minutes
+                int intInterval = monFrequency * 60000;
+                // find process by name
+                Process[] procs = Process.GetProcessesByName(args[0]);
 
-                //if there are no proccesses with this name log it in console
+                // if there are no processes with this name log it to console
                 if (procs.Length == 0)
                 {
                     Console.WriteLine($"{DateTime.Now} - Can't find processes named '{args[0]}'");
                 }
-                //if monitor find any proccesses log their info in console
+                // if monitor find any processes log their info to console
                 else if (procs.Length > 0)
                 {
                     foreach (var proc in procs)
                     {
                         var lifetime = DateTime.Now - proc.StartTime;
 
-                        System.Console.WriteLine($"{DateTime.Now} - {proc.ProcessName} #{proc.Id} - {lifetime.TotalMinutes}");
+                        Console.WriteLine($"{DateTime.Now} - {proc.ProcessName} #{proc.Id} - {lifetime.TotalMinutes:0.000}m");
 
-                        if (lifetime.TotalMinutes > maxLifeTime) //kill the process if it takes too much time
+                        // kill the process if it takes too much time
+                        if (lifetime.TotalMinutes > maxLifeTime)
                         {
                             proc.Kill();
-                            Console.WriteLine($"{DateTime.Now} - Proccess '{proc.ProcessName}' #{proc.Id} was killed.");
+                            Console.WriteLine($"{DateTime.Now} - Process '{proc.ProcessName}' #{proc.Id} has been killed.");
                         }
                     }
                 }
                 Thread.Sleep(intInterval);
             }
 
-            System.Console.WriteLine($"Monitor for proccess '{args[0]}' was stopped...");
+            Console.WriteLine($"Monitor for process '{args[0]}' has been stopped...");
         }
     }
 }
